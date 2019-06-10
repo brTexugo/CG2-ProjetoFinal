eb99388fdf52dc69d1586b0d68fbeb2afbf5d608
Gabriel <Gabriel@Gabriel-PC>
2019-06-10 20:36:37 -0300

    100619

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -1,7 +1,26 @@
 package mygame;
 
+
+import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.control.CharacterControl;
+
+import com.jme3.bullet.util.CollisionShapeFactory;
+
+import com.jme3.input.KeyInput;
+
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
+
+import com.jme3.math.ColorRGBA;
+
+import com.jme3.scene.Spatial;
+
 import com.jme3.app.SimpleApplication;
 import com.jme3.asset.TextureKey;
+import com.jme3.audio.AudioData.DataType;
+import com.jme3.audio.AudioNode;
 import com.jme3.bullet.BulletAppState;
 import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.font.BitmapText;
@@ -12,6 +31,7 @@ import com.jme3.material.Material;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
 import com.jme3.scene.shape.Sphere;
 import com.jme3.scene.shape.Sphere.TextureMode;
@@ -19,20 +39,32 @@ import com.jme3.texture.Texture;
 import com.jme3.texture.Texture.WrapMode;
 
 /**
- * Example 12 - how to give objects physical properties so they bounce and fall.
- * @author base code by double1984, updated by zathras
+ * Example 9 - How to make walls and floors solid.
+ * This collision code uses Physics and a custom Action Listener.
+ * @author normen, with edits by Zathras
  */
-public class Main extends SimpleApplication {
-
-  public static void main(String args[]) {
-     Main app = new Main();
-    app.start();
-  }
-
-  /** Prepare the Physics Application State (jBullet) */
+public class Main extends SimpleApplication
+        implements ActionListener {
+  private AudioNode audio_gun;  
+  private AudioNode audio_nature; 
+  private Spatial sceneModel;
   private BulletAppState bulletAppState;
+  private RigidBodyControl landscape;
+  private CharacterControl player;
+  private Vector3f walkDirection = new Vector3f();
+  private boolean left = false, right = false, up = false, down = false;
 
-  /** Prepare Materials */
+    
+  private float elefVelocidade = 5f;
+  
+  private float ultimoTempo = 0f;
+  private float dificuldadeT = 0f;
+  
+  private Vector3f camDir = new Vector3f();
+  private Vector3f camLeft = new Vector3f();
+  private Node elefante;
+  Node player1;
+  
   Material wall_mat;
   Material stone_mat;
   Material floor_mat;
@@ -46,124 +78,104 @@ public class Main extends SimpleApplication {
   private static final Box    floor;
 
   /** dimensions used for bricks and wall */
-  private static final float brickLength = 0.50f;
-  private static final float brickWidth  = 0.30f;
-  private static final float brickHeight = 0.20f;
+  private static final float brickLength = 0.48f;
+  private static final float brickWidth  = 0.24f;
+  private static final float brickHeight = 0.12f;
 
   static {
     /** Initialize the cannon ball geometry */
     sphere = new Sphere(32, 32, 0.4f, true, false);
     sphere.setTextureMode(TextureMode.Projected);
     /** Initialize the brick geometry */
-    box = new Box(Vector3f.ZERO, brickLength, brickHeight, brickWidth);
+    box = new Box(brickLength, brickHeight, brickWidth);
     box.scaleTextureCoordinates(new Vector2f(1f, .5f));
     /** Initialize the floor geometry */
-    floor = new Box(Vector3f.ZERO, 10f, 0.1f, 5f);
+    floor = new Box(10f, 0.1f, 5f);
     floor.scaleTextureCoordinates(new Vector2f(3, 6));
   }
 
-  @Override
-  public void simpleInitApp() {
-    /** Set up Physics Game */
-    bulletAppState = new BulletAppState();
-    stateManager.attach(bulletAppState);
-    //bulletAppState.getPhysicsSpace().enableDebug(assetManager);
 
-    /** Configure cam to look at scene */
-    cam.setLocation(new Vector3f(0, 4f, 6f));
-    cam.lookAt(new Vector3f(2, 2, 0), Vector3f.UNIT_Y);
-    /** Add InputManager action: Left click triggers shooting. */
-    inputManager.addMapping("shoot", 
-            new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
-    inputManager.addListener(actionListener, "shoot");
-    /** Initialize the scene, materials, and physics space */
-    initMaterials();
-    initWall();
-    initFloor();
-    initCrossHairs();
+  public static void main(String[] args) {
+    Main app = new Main();
+    app.start();
   }
 
-  /**
-   * Every time the shoot action is triggered, a new cannon ball is produced.
-   * The ball is set up to fly from the camera position in the camera direction.
-   */
+  public void simpleInitApp() {
+     
+    bulletAppState = new BulletAppState();
+    stateManager.attach(bulletAppState);
+
+    viewPort.setBackgroundColor(new ColorRGBA(0.7f, 0.8f, 1f, 1f));
+    flyCam.setMoveSpeed(100);
+    setUpKeys();
+    setUpLight();
+
+    sceneModel = assetManager.loadModel("Models/town/main.scene");
+    sceneModel.setLocalScale(2f);
+
+    CollisionShape sceneShape =
+            CollisionShapeFactory.createMeshShape(sceneModel);
+    landscape = new RigidBodyControl(sceneShape, 0);
+    sceneModel.addControl(landscape);
+
+    CapsuleCollisionShape capsuleShape = new CapsuleCollisionShape(1.5f, 6f, 1);
+    player = new CharacterControl(capsuleShape, 0.05f);
+    player.setJumpSpeed(20);
+    player.setFallSpeed(30);
+
+    player.setGravity(new Vector3f(0,-30f,0));
+
+    player.setPhysicsLocation(new Vector3f(0, 10, 0));
+
+    inputManager.addMapping("shoot",
+        new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
+    inputManager.addListener(actionListener, "shoot");
+   
+    initMaterials();
+    initAudio();
+    initCrossHairs();
+    player1 = (Node) assetManager.loadModel("Models/Elephant/Elephant.mesh.xml");
+    player1.setLocalScale(0.08f);
+    player1.setLocalTranslation(-20,1,0);
+    rootNode.attachChild(player1);
+    
+    rootNode.attachChild(sceneModel);
+    bulletAppState.getPhysicsSpace().add(landscape);
+    bulletAppState.getPhysicsSpace().add(player);
+  }
   private ActionListener actionListener = new ActionListener() {
     public void onAction(String name, boolean keyPressed, float tpf) {
       if (name.equals("shoot") && !keyPressed) {
         makeCannonBall();
+        audio_gun.playInstance();
       }
     }
   };
-
-  /** Initialize the materials used in this scene. */
-  public void initMaterials() {
-    wall_mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-    TextureKey key = new TextureKey("Textures/Terrain/BrickWall/BrickWall.jpg");
-    key.setGenerateMips(true);
-    Texture tex = assetManager.loadTexture(key);
-    wall_mat.setTexture("ColorMap", tex);
-
+    private void initAudio() {
+    /* gun shot sound is to be triggered by a mouse click. */
+    audio_gun = new AudioNode(assetManager, "Sound/Effects/Bang.wav", DataType.Buffer);
+    audio_gun.setPositional(false);
+    audio_gun.setLooping(false);
+    audio_gun.setVolume(2);
+    rootNode.attachChild(audio_gun);
+    
+    audio_nature = new AudioNode(assetManager, "Sounds/sup1.ogg", DataType.Stream);
+    audio_nature.setLooping(true);  // activate continuous playing
+    audio_nature.setPositional(true);
+    audio_nature.setVolume(3);
+    rootNode.attachChild(audio_nature);
+    audio_nature.play();
+   }
+   public void initMaterials() {
     stone_mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
     TextureKey key2 = new TextureKey("Textures/Terrain/Rock/Rock.PNG");
     key2.setGenerateMips(true);
     Texture tex2 = assetManager.loadTexture(key2);
     stone_mat.setTexture("ColorMap", tex2);
-
-    floor_mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-    TextureKey key3 = new TextureKey("Textures/Terrain/Pond/Pond.jpg");
-    key3.setGenerateMips(true);
-    Texture tex3 = assetManager.loadTexture(key3);
-    tex3.setWrap(WrapMode.Repeat);
-    floor_mat.setTexture("ColorMap", tex3);
   }
 
-  /** Make a solid floor and add it to the scene. */
-  public void initFloor() {
-    Geometry floor_geo = new Geometry("Floor", floor);
-    floor_geo.setMaterial(floor_mat);
-    floor_geo.setLocalTranslation(0, -0.1f, 0);
-    this.rootNode.attachChild(floor_geo);
-    /* Make the floor physical with mass 0.0f! */
-    floor_phy = new RigidBodyControl(0.0f);
-    floor_geo.addControl(floor_phy);
-    bulletAppState.getPhysicsSpace().add(floor_phy);
-  }
-
-  /** This loop builds a wall out of individual bricks. */
-  public void initWall() {
-    float startpt = brickLength / 4;
-    float height = 0;
-    for (int j = 0; j < 15; j++) {
-      for (int i = 0; i < 6; i++) {
-        Vector3f vt =
-         new Vector3f(i * brickLength * 2 + startpt, brickHeight + height, 0);
-        makeBrick(vt);
-      }
-      startpt = -startpt;
-      height += 2 * brickHeight;
-    }
-  }
-
-  /** This method creates one individual physical brick. */
-  public void makeBrick(Vector3f loc) {
-    /** Create a brick geometry and attach to scene graph. */
-    Geometry brick_geo = new Geometry("brick", box);
-    brick_geo.setMaterial(wall_mat);
-    rootNode.attachChild(brick_geo);
-    /** Position the brick geometry  */
-    brick_geo.setLocalTranslation(loc);
-    /** Make brick physical with a mass > 0.0f. */
-    brick_phy = new RigidBodyControl(2f);
-    /** Add physical brick to physics space. */
-    brick_geo.addControl(brick_phy);
-    bulletAppState.getPhysicsSpace().add(brick_phy);
-  }
-
-  /** This method creates one individual physical cannon ball.
-   * By defaul, the ball is accelerated and flies
-   * from the camera position in the camera direction.*/
    public void makeCannonBall() {
-    /** Create a cannon ball geometry and attach to scene graph. */
+  
     Geometry ball_geo = new Geometry("cannon ball", sphere);
     ball_geo.setMaterial(stone_mat);
     rootNode.attachChild(ball_geo);
@@ -178,7 +190,6 @@ public class Main extends SimpleApplication {
     ball_phy.setLinearVelocity(cam.getDirection().mult(25));
   }
 
-  /** A plus sign used as crosshairs to help the player with aiming.*/
   protected void initCrossHairs() {
     guiNode.detachAllChildren();
     guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
@@ -190,4 +201,100 @@ public class Main extends SimpleApplication {
       settings.getHeight() / 2 + ch.getLineHeight() / 2, 0);
     guiNode.attachChild(ch);
   }
-}
+
+  private void setUpLight() {
+  
+    AmbientLight al = new AmbientLight();
+    al.setColor(ColorRGBA.White.mult(1.3f));
+    rootNode.addLight(al);
+
+    DirectionalLight dl = new DirectionalLight();
+    dl.setColor(ColorRGBA.White);
+    dl.setDirection(new Vector3f(2.8f, -2.8f, -2.8f).normalizeLocal());
+    rootNode.addLight(dl);
+  }
+
+  private void setUpKeys() {
+    inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
+    inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
+    inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_W));
+    inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
+    inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
+    inputManager.addListener(this, "Left");
+    inputManager.addListener(this, "Right");
+    inputManager.addListener(this, "Up");
+    inputManager.addListener(this, "Down");
+    inputManager.addListener(this, "Jump");
+  }
+
+  public void onAction(String binding, boolean isPressed, float tpf) {
+    if (binding.equals("Left")) {
+      left = isPressed;
+    } else if (binding.equals("Right")) {
+      right= isPressed;
+    } else if (binding.equals("Up")) {
+      up = isPressed;
+    } else if (binding.equals("Down")) {
+      down = isPressed;
+    } else if (binding.equals("Jump")) {
+ 
+      if (isPressed) { player.jump(new Vector3f(0,20f,0));}
+    }
+    if(timer.getTimeInSeconds() > ultimoTempo + 5){
+            ultimoTempo = timer.getTimeInSeconds();
+            elefante.attachChild(makeElefante());
+        }
+        
+        if(timer.getTimeInSeconds() > dificuldadeT + 5 && elefVelocidade <= 20){
+            dificuldadeT = timer.getTimeInSeconds();
+            elefVelocidade = elefVelocidade+ 5;
+        }
+    
+  }
+    
+  protected Spatial makeElefante() {
+
+    Spatial elef = assetManager.loadModel("Models/Elephant/Elephant.mesh.xml");
+    elef.scale(0.08f);
+    elef.setLocalTranslation(2.0f, 1f, 2f);
+    return elef;
+  }
+  @Override
+    public void simpleUpdate(float tpf) {
+        camDir.set(cam.getDirection()).multLocal(0.6f);
+        camLeft.set(cam.getLeft()).multLocal(0.4f);
+        walkDirection.set(0, 0, 0);
+        if (left) {
+            walkDirection.addLocal(camLeft);
+        }
+        if (right) {
+            walkDirection.addLocal(camLeft.negate());
+        }
+        if (up) {
+            walkDirection.addLocal(camDir);
+        }
+        if (down) {
+            walkDirection.addLocal(camDir.negate());
+        }
+        player.setWalkDirection(walkDirection);
+        
+        player1.setLocalTranslation(0,tpf,0);
+        
+       /*for(Spatial s : elefante.getChildren()){
+            s.lookAt(cam.getLocation(),  Vector3f.UNIT_Y.normalize());
+            s.rotate(0, (float) Math.PI , 0);
+            Vector3f dir = s.getLocalTranslation().subtract(cam.getLocation()).normalize().negate();
+            System.out.println(cam.getLocation().subtract(s.getLocalTranslation()).getX());
+       if(cam.getLocation().subtract(s.getLocalTranslation()).getX() < 5 && cam.getLocation().subtract(s.getLocalTranslation()).getX() > -5
+               && cam.getLocation().subtract(s.getLocalTranslation()).getY() < 5 && cam.getLocation().subtract(s.getLocalTranslation()).getY() > -5){
+           s.move(dir.x*0*tpf, 0, dir.z*0*tpf);  
+        }
+       else{
+           s.move(dir.x*elefVelocidade*tpf, 0, dir.z*elefVelocidade*tpf);
+        }  
+        }*/
+        cam.setLocation(player.getPhysicsLocation());
+        listener.setLocation(cam.getLocation());
+        listener.setRotation(cam.getRotation());
+    }
+ }
